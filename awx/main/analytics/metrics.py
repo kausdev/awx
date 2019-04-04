829316eb06be4a405016c35a4d017e747259e460
Wayne Witzel III <wayne@riotousliving.com>
2019-04-04 10:12:33 -0400

    adjust metrics to use labels

--- awx/main/analytics/metrics.py
+++ awx/main/analytics/metrics.py
@@ -25,21 +25,19 @@ from awx.main.analytics import register
 
 SYSTEM_INFO = Info('awx_system', 'AWX System Information')
 
-ORG_COUNT = Gauge('awx_organizations', 'Number of organizations')
-USER_COUNT = Gauge('awx_users', 'Number of users')
-TEAM_COUNT = Gauge('awx_teams', 'Number of teams')
-INV_COUNT = Gauge('awx_inventories', 'Number of inventories')
-PROJ_COUNT = Gauge('awx_projects', 'Number of projects')
-JT_COUNT = Gauge('awx_job_templates', 'Number of job templates')
-WFJT_COUNT = Gauge('awx_workflow_job_templates', 'Number of workflow job templates')
-HOST_COUNT = Gauge('awx_hosts', 'Number of hosts')
-SCHEDULE_COUNT = Gauge('awx_schedules', 'Number of schedules')
-INV_SCRIPT_COUNT = Gauge('awx_inventory_scripts', 'Number of invetory scripts')
-TOTAL_SESSIONS = Gauge('awx_active_sessions', 'Number of active session')
-CUSTOM_VENVS = Gauge('awx_custom_virtualenvs', 'Number of virtualenvs')
-ACTIVE_USER_SESSIONS = Gauge('awx_active_user_sessions', 'Number of Tower users logged in')
-RUNNING_JOBS = Gauge('awx_running_jobs', 'Number of running jobs on the Tower system')
-ACTIVE_HOST_COUNT = Gauge('awx_active_hosts', 'Number of active hosts counting towards subscription count')
+ORG_COUNT = Gauge('awx_organizations_total', 'Number of organizations')
+USER_COUNT = Gauge('awx_users_total', 'Number of users')
+TEAM_COUNT = Gauge('awx_teams_total', 'Number of teams')
+INV_COUNT = Gauge('awx_inventories_total', 'Number of inventories')
+PROJ_COUNT = Gauge('awx_projects_total', 'Number of projects')
+JT_COUNT = Gauge('awx_job_templates_total', 'Number of job templates')
+WFJT_COUNT = Gauge('awx_workflow_job_templates_total', 'Number of workflow job templates')
+HOST_COUNT = Gauge('awx_hosts_total', 'Number of hosts', ['type',])
+SCHEDULE_COUNT = Gauge('awx_schedules_total', 'Number of schedules')
+INV_SCRIPT_COUNT = Gauge('awx_inventory_scripts_total', 'Number of invetory scripts')
+USER_SESSIONS = Gauge('awx_sessions_total', 'Number of sessions', ['type',])
+CUSTOM_VENVS = Gauge('awx_custom_virtualenvs_total', 'Number of virtualenvs')
+RUNNING_JOBS = Gauge('awx_running_jobs_total', 'Number of running jobs on the Tower system')
 
 def metrics():
     license_info = get_license(show_key=False)
@@ -63,15 +61,19 @@ def metrics():
     PROJ_COUNT.set(current_counts['project'])
     JT_COUNT.set(current_counts['job_template'])
     WFJT_COUNT.set(current_counts['workflow_job_template'])
-    HOST_COUNT.set(current_counts['host'])
+
+    HOST_COUNT.labels(type='all').set(current_counts['host'])
+    HOST_COUNT.labels(type='active').set(current_counts['active_host_count'])
+
     SCHEDULE_COUNT.set(current_counts['schedule'])
     INV_SCRIPT_COUNT.set(current_counts['custom_inventory_script'])
     CUSTOM_VENVS.set(current_counts['custom_virtualenvs'])
-    TOTAL_SESSIONS.set(current_counts['active_sessions'])
-    ACTIVE_USER_SESSIONS.set(current_counts['active_user_sessions'])
-    RUNNING_JOBS.set(current_counts['running_jobs'])
-    ACTIVE_HOST_COUNT.set(current_counts['active_host_count'])
 
+    USER_SESSIONS.labels(type='all').set(current_counts['active_sessions'])
+    USER_SESSIONS.labels(type='user').set(current_counts['active_user_sessions'])
+    USER_SESSIONS.labels(type='anonymous').set(current_counts['active_anonymous_sessions'])
+
+    RUNNING_JOBS.set(current_counts['running_jobs'])
 
     return generate_latest()
 
